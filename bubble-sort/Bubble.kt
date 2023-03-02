86dcee79f06fef018d3687d6abf8bcc2dd708b43
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-03-01 22:34:18 -0500

    example: BubbleSort (Kotlin)

---
+++ bubble-sort/Bubble.kt
@@ -0,0 +1,17 @@
+fun bubble(arr: IntArray): IntArray {
+    for (i in 0..arr.lastIndex) {
+        for (j in 0..arr.lastIndex - 1) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+    return arr
+}
+
+fun main() {
+    val arr = bubble(intArrayOf(12, 31, 25, 8, 32, 17))
+    println(arr.contentToString())
+}
