b3631bd9542e3bc01618e482ec0e63be39c25863
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-03-04 11:08:58 -0500

    fix: InsertionSort (Python)

--- insertion-sort/Insertion.py
+++ insertion-sort/Insertion.py
@@ -2,7 +2,6 @@ import time
 
 
 def insertion(arr):
-    start = time.time()
     i = 1
     for j in range(0, len(arr) - 1):
         current = arr[i]
@@ -11,9 +10,10 @@ def insertion(arr):
             j -= 1
         arr[j + 1] = current
         i += 1
-    end = time.time()
-
-    print(arr, "time: " + str((end - start) * 10**3))
+    return arr
 
 
-insertion([5, 21, 44, 33, 18, 10])
+start = time.time()
+arr = insertion([5, 21, 44, 33, 18, 10])
+end = time.time()
+print(arr, "time: " + str((end - start) * 10**3))
